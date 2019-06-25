87943b900a10b9c2a6b630c9082dc1a4152195cb
Bruno Camacho <bruno.v.camacho@gmail.com>
2019-06-25 17:37:04 -0300

    Criacao do login e logout do admin

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php 
-
+    
+    session_start();
     require_once("vendor/autoload.php");
 
     use Slim\Slim;
     use BrunoCamacho\Page;
     use BrunoCamacho\PageAdmin;
+    use BrunoCamacho\Model\User;
     
     $app = new Slim();
     $app->config('debug', true);
 
     $app->get('/', function() {
+        
         
         $page = new Page();
         $page->setTpl("index");
@@ -18,10 +21,32 @@
 
     });
     
-    $app->get('/admin', function(){
+    $app->get('/admin/', function(){
+        $user = User::verifyLogin();
         $page = new PageAdmin();
         $page->setTpl("index");
     
+    });
+    
+    $app->get('/admin/login/', function(){
+        $page = new PageAdmin([
+            "header"=>false,
+            "footer"=>false
+        ]);
+        $page->setTpl("login");
+    });
+    
+    
+    $app->post('/admin/login/', function(){
+        User::login($_POST['login'], $_POST['password']);
+        header("Location: /admin");
+        exit;
+    });
+    
+    $app->get('/admin/logout/', function(){
+        User::logout();
+        header("Location: /admin/login");
+        exit;
     });
 
     $app->run();
